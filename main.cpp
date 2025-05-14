9d8e124f50c6e0e9d83dc1d31d7609e27bb90b8c
rvzvr7 <ptr.kozlowski@wp.pl>
2025-05-14 23:33:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <QApplication>
+#include <QCoreApplication>
+#include <QDebug>
 #include "mainwindow.h"
 #include "logger/logger.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    QCoreApplication::setOrganizationName("Projekt_JPO");
+    QCoreApplication::setApplicationName("MonitorJakosciPowietrza");
 
     try {
         Logger::init();
